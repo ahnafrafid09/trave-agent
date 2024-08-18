560b770d113eb79c32f692532c0346de4f8a7e85
Ahnaf <Ahnafrafid22@gmail.com>
2024-08-19 00:01:08 +0700

    add whatsapp icon

--- src/components/WhatsappIcon.jsx
+++ src/components/WhatsappIcon.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsappIcon = () => {
   return (
     <a
-      href="https://wa.me/6282244087426?text=Halo%20Mayasari%20Garage,%20saya%20ingin%20bertanya"
+      href="https://wa.me/6282244087426?text=Halo%20Anjangsana%20Trip%20PlannerGarage,%20saya%20ingin%20bertanya"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-8 right-8 z-50 p-2 bg-green-600 rounded-full shadow-lg hover:bg-green-800 transition-colors duration-300"
