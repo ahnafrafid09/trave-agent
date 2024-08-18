44d0a0fdfd688d9107a1a4d6a0847ae06d6fde45
Ahnaf <Ahnafrafid22@gmail.com>
2024-08-19 00:01:50 +0700

    update halaman produk

--- src/pages/produkKami/ProdukKami.jsx
+++ src/pages/produkKami/ProdukKami.jsx
@@ -1,34 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "../../components/Header";
 import Card from "../../components/Card";
-import katalogMobil from "../../translate/id/katalogMobil.json";
+import { useTranslation } from "react-i18next";
 
 const ProdukKami = () => {
-  const [datas, setDatas] = useState([]);
-
-  useEffect(() => {
-    setDatas(katalogMobil);
-  }, []);
+  const { t } = useTranslation("produk");
+  const katalogMobil = t("katalogMobil", { returnObjects: true });
+  const listMobil = katalogMobil?.listMobil || [];
+  const label = katalogMobil.labels;
 
   return (
     <section className="mt-16 md:mt-24">
-      <Header>Produk Kami</Header>
-      <div className="container mx-auto">
+      <Header>{t("heading.title")}</Header>
+      <div className="container">
         <h1 className="text-2xl md:text-3xl font-bold mb-6 mt-12 text-center md:text-start">
-          Price List Sewa Mobil
+          {t("katalogMobil.title")}
         </h1>
         <div className="flex justify-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-8 lg:gap-12 md:px-4 lg:px-0 max-w-6xl">
-            {datas.map((data) => (
+            {listMobil.map((data) => (
               <Card
                 key={data.id}
                 gambar={data.gambar}
+                judul={data.namaMobil}
                 namaMobil={data.namaMobil}
-                tahun={data.tahun}
-                bahanBakar={data.bahanBakar}
-                jenisMobil={data.jenisMobil}
-                kapasitasPenumpang={data.kapasitasPenumpang}
-                harga={data.harga}
+                harga={data.harga.toLocaleString()}
+                details={[
+                  { label: label.tahun, value: data.tahun },
+                  { label: label.jenisMobil, value: data.jenisMobil },
+                  { label: label.bahanBakar, value: data.bahanBakar },
+                  {
+                    label: label.kapasitas,
+                    value: `${data.kapasitasPenumpang} + ${data.kapasitasDriver}`,
+                  },
+                ]}
               />
             ))}
           </div>
